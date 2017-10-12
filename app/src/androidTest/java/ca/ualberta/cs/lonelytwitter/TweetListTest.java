046b699f0b8e8e22e9f68465feb811711a045329
zhangyi921 <yi16@ualberta.ca>
2017-10-11 19:43:19 -0600

    add method

---
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
@@ -0,0 +1,10 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * Created by yi16 on 10/11/17.
+ */
+
+public class TweetListTest extends ActivityInstrumentationTestCase2 {
+}
---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
@@ -0,0 +1,8 @@
+package ca.ualberta.cs.lonelytwitter;
+
+/**
+ * Created by yi16 on 10/11/17.
+ */
+
+public class TweetList {
+}
